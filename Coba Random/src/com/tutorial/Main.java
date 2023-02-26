f396e2c2090b3d295260994f4c1947acac284b00
KLaZ <muhabdanulikhlas0983@gmail.com>
2023-02-26 21:45:21 +0700

    [CREATE] membuat folder Coba Random

---
+++ Coba Random/src/com/tutorial/Main.java
@@ -0,0 +1,23 @@
+package com.tutorial;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("\n"); // ? newline
+
+        // ? main code
+        // System.out.println("Hello World");
+        // System.out.print("Hai ");
+        // System.out.println("Hai");
+        // int nilaiInt = 450;
+        // System.out.println(nilaiInt);
+
+        // ! Komparasi / logika
+        int a = 10;
+        int b = 20;
+        boolean hasil = (a == b);
+        System.out.printf("apakah %d sama dengan %d ? %s", a, b, hasil);
+
+        System.out.println("\n"); // ? newline
+    }
+
+}
