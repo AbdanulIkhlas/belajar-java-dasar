910f5f062957aad28fd4fad27e664a5b494313e3
KLaZ <muhabdanulikhlas0983@gmail.com>
2023-02-26 22:24:27 +0700

    [UPDATE] menambahkan informasi operator logika pada Main.java

--- Komparasi Dan Operator/src/Main.java
+++ Komparasi Dan Operator/src/Main.java
@@ -1,13 +1,37 @@
+
 public class Main {
 
     public static void main(String[] args) {
         // ! komparasi logika
         System.out.println("Starting\n");
 
-        int a = 10, b = 10;
-        boolean hasil = (a == b);
+        // int a = 10, b = 10;
+        // boolean hasil = (a == b);
 
-        System.out.println(a + " == " + b + " ---> " + hasil);
+        // System.out.println(a + " == " + b + " ---> " + hasil);
+
+        // ! Operator LOGIKA ::::: Semua sama seperti C++ ada && , ||, !
+        // ? Di tambah satu XOR (^)
+        boolean a, b, hasil;
+        a = true;
+        b = true;
+        hasil = a ^ b;
+        System.out.println(a + " ^ " + b + " ==> " + hasil);
+
+        a = true;
+        b = false;
+        hasil = a ^ b;
+        System.out.println(a + " ^ " + b + " ==> " + hasil);
+
+        a = false;
+        b = true;
+        hasil = a ^ b;
+        System.out.println(a + " ^ " + b + " ==> " + hasil);
+
+        a = false;
+        b = false;
+        hasil = a ^ b;
+        System.out.println(a + " ^ " + b + " ==> " + hasil);
 
         System.out.println("\nEnding");
     }
