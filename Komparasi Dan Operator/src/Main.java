9a0c8330b01f9ff6aebe2c3bbe71b51f4a33521a
KLaZ <muhabdanulikhlas0983@gmail.com>
2023-02-26 22:02:32 +0700

    [CREATE] membuat file 'Komparasi dan Operator'

---
+++ Komparasi Dan Operator/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+    public static void main(String[] args) {
+        // ! komparasi logika
+        System.out.println("Starting\n");
+
+        int a = 10, b = 10;
+        boolean hasil = (a == b);
+
+        System.out.println(a + " == " + b + " ---> " + hasil);
+
+        System.out.println("\nEnding");
+    }
+}
